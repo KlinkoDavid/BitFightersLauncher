9bc3544b1b559d10a1cd66a1584e9fc5f97bfe8a
KlinkoDavid <klinko.david@simonyiszki.org>
2025-08-22 10:59:49 +0200

    Ujjitás

--- BitFightersLauncher/MainWindow.xaml.cs
+++ BitFightersLauncher/MainWindow.xaml.cs
@@ -1,19 +1,8 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BitFightersLauncher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -21,9 +10,17 @@ namespace BitFightersLauncher
             InitializeComponent();
         }
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        private void DragWindow(object sender, MouseButtonEventArgs e)
         {
-            this.Close();
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
